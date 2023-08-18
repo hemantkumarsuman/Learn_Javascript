c5bcb0cac25283e1d9c60a3a38c4f41014063541
hemantkumarsuman <93324944+hemantkumarsuman@users.noreply.github.com>
2023-08-18 17:59:33 +0000

    conversion_step1

---
+++ Basics/03_Datatypesconversion.js
@@ -0,0 +1,19 @@
+let score1=35
+let score2="34"
+
+console.log(typeof score1);
+console.log(typeof score2);
+//convert string 34 to number 234
+let score2Num=Number(score2);
+console.log(typeof score2Num);
+//Number conversion
+// "33"-> 33
+//"33abc"->NaN
+//true -> 1/false->0
+
+//Boolean  coversion
+
+//1->true;0->false
+//""->false
+//"Hemant"->true
+
