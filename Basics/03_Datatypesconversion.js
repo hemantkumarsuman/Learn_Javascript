a169012253e841a806821b22f3d7720cedb4f563
hemantkumarsuman <93324944+hemantkumarsuman@users.noreply.github.com>
2023-08-19 12:11:18 +0000

    comparision

--- Basics/03_Datatypesconversion.js
+++ Basics/03_Datatypesconversion.js
@@ -1,11 +1,11 @@
 let score1=35
 let score2="34"
 
-console.log(typeof score1);
-console.log(typeof score2);
+// console.log(typeof score1);
+// console.log(typeof score2);
 //convert string 34 to number 234
 let score2Num=Number(score2);
-console.log(typeof score2Num);
+//console.log(typeof score2Num);
 //Number conversion
 // "33"-> 33
 //"33abc"->NaN
@@ -19,5 +19,36 @@ console.log(typeof score2Num);
 /*-------------------------------------------------------------------------------*/
 
 //OPERATIONS 
+let value=3
+let negValue=-value;
+// console.log(negValue);
+// console.log(3*3)//multiplication 
+
+// console.log(3**3)//power
+// console.log(34%5)//remeinder
+
+let str1="Hello"
+let str2=" Hemant"
+let ans=str1+str2;
+// console.log(ans);
+
+// console.log(1+"1");
+
+// console.log("2"+2);
+
+// console.log(2+1+"1");
+
+// console.log("2"+2+3);
+
+// console.log(true);
+// console.log(+true);
+
+// console.log((true+));//error
+
+let val,val2,val3;
+val1=val2=val3=2+2;//all variable stores 4
+ let gameScore=45;
+ gameScore++;
+ console.log(gameScore)
 
 
