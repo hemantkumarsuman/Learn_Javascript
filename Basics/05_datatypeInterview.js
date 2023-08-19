ab1c03b098d2eeda80c06cdc1c7c78454b9a7ed2
hemantkumarsuman <93324944+hemantkumarsuman@users.noreply.github.com>
2023-08-19 12:41:41 +0000

    datatype summary

---
+++ Basics/05_datatypeInterview.js
@@ -0,0 +1,25 @@
+// Category of Data types: 1.Primitive   2.Non-primitive
+// These categorization occur on the basis of memory allocation and accessing the data
+
+//#Primitive -> call by value
+
+//1.number 2.string 3.boolean 4.null 5.undefined 6.symbol 7.BigInt  
+
+//#Other data type(non-primitive) -> These r reference data type
+
+//1.arrays 2.objects 3.functions
+//const hero=["shakiman","krish","naagraj"]->arrays
+
+//function
+const myFunc =function(){
+    console.log("Hello World");
+}
+console.log(myFunc);
+
+console.log(typeof null)//object
+
+console.log(typeof myFunc)// object function
+//JS is dynamically typed language becaz datatype will automatically assigned at time of compilation or code execution
+
+
+
