9f8e3af80c14fe6aa6df9141afb3763c69fa0828
hemantkumarsuman <93324944+hemantkumarsuman@users.noreply.github.com>
2023-08-20 09:19:31 +0000

    string

---
+++ Basics/06_stackHeapMemory.js
@@ -0,0 +1,18 @@
+//stack(for all primitive data type),Heap(Non-primitive)
+
+let myName="Hemant";
+
+let anotherName=myName;
+
+// console.log(myName)
+// console.log(anotherName);
+
+let userOne={
+
+    name:"hemant",
+    upi:"upi@okhdfc"
+}
+let userTwo=userOne;
+console.log(userOne)
+console.log(userTwo);
+
