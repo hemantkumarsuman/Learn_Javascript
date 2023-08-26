969d29b990b417fd0dc28d9dbe4315d2c9ffd7f9
hemantkumarsuman <93324944+hemantkumarsuman@users.noreply.github.com>
2023-08-26 10:34:47 +0000

    Date and Time

---
+++ Basics/09_DateAndTime.js
@@ -0,0 +1,24 @@
+//Date and Time -> It is object in JS
+
+let date=new Date();
+// console.log(date);
+// console.log(typeof date);
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toJSON());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleTimeString());
+
+let newDate=new Date();
+// console.log(newDate);
+// console.log(newDate.getDay());
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getFullYear());
+// console.log(newDate.getDate());
+
+
+let date1=Date.now();
+console.log(date1);//gives date in millisecond->Alway usefull when we want to compare dates 
+
