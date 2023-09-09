1e4c9fd4a471f832afe4f41bb29a8ab4faf2fd52
hemantkumarsuman <93324944+hemantkumarsuman@users.noreply.github.com>
2023-09-09 08:01:27 +0000

    control flow

---
+++ 04_controlFlow/01_one.js
@@ -0,0 +1,85 @@
+//IF statement
+//compare opreator=> <,>,<=,>=,==,!=,===
+// const isUserLoggedIn=false;
+// if(isUserLoggedIn)
+// {
+//     console.log("Yes");
+// }
+// else{
+//     console.log("No");
+// }
+
+
+//switch case
+// const month=7;
+// switch(month)
+// {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+//         break;
+//     case 5:
+//         console.log("May");
+//         break;
+//     case 6:
+//         console.log("June");
+//         break;
+//     case 7:
+//         console.log("July");
+//         break;
+//     case 8:
+//         console.log("August");
+//         break;
+//     case 9:
+//         console.log("September");
+//         break;
+//     case 10:
+//         console.log("October");
+//         break;
+//     case 11:
+//         console.log("November");
+//         break;
+//     case 12:
+//         console.log("December");
+//         break;
+//     default:
+//         console.log("Not a month");
+// }
+
+// falsy value 
+// false, 0, -0, "", null, undefined, NaN
+
+//Truthy value
+// "0", 'false', " ",[], {}, function(){}, 
+
+// How to check Array and Object
+// const arr=[]
+// if(arr.length===0)
+// {
+//     console.log(`${arr} is empty`);
+
+// }
+
+// const emptyObj={}
+// if(Object.keys(emptyObj).length===0)
+// {
+//     console.log(`${emptyObj} is empty`);
+// }
+
+// if(false==0) output=true
+// if(false=='') output=true
+// if(false==[]) output=true
+
+//Ternary Operator
+// condition ? true : false
+
+const iceTeaPrice=100
+iceTeaPrice >=50 ? console.log("Greater than 50"): console.log("Less than equal to 50");
